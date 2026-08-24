fb02e05c6255070afa58893522f88408bcd37360
Prince <princerawat7700@gmail.com>
2026-08-24 20:05:09 +0530

    improve database connection handling in Prisma setup

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,8 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
 });
 
 export const prisma = new PrismaClient({
